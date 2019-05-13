31d27226eb8671de536aefab077f8b8e54f7cce4
Alex Fullalove <alexanderfullalove@gmail.com>
2019-05-13 14:35:40 +0100

    created header component and basic styling

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <header id="title">Nc News</header>
+    </div>
+  );
+};
+
+export default Header;
