5946ca47d071ac106fae594c56757675ee9e0bd8
Alex Fullalove <alexanderfullalove@gmail.com>
2019-05-16 21:08:50 +0100

    updated header

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <div className="header">
-      <header id="title">Nc News</header>
+      <header id="title">NC News</header>
     </div>
   );
 };
